26b01c9bc5e9b68c771925c5224c54d021b1e89f
aslan-turk <asevketaslanturk@gmail.com>
2023-02-17 06:59:39 +0300

    qwe

---
+++ src/components/Main/Card.jsx
@@ -0,0 +1,21 @@
+const Card = ({ id, title, date, desc, image }) => {
+  return (
+    <div className="cards">
+      <div className="title">
+        <h1>{title}</h1>
+      </div>
+
+      <div className="date">
+        <h1>{date}</h1>
+      </div>
+
+      <img src={image} alt="" />
+
+      <div className="card-over">
+        <p>{desc}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
